8bf750e74c6ac05bccf9e4b66d005ed92d38dbe0
Minh Le <minh.le@c0x12c.com>
2024-06-13 23:22:47 +0700

    UI: enhancement

--- components/logo.tsx
+++ components/logo.tsx
@@ -16,12 +16,12 @@ export const Logo = () => {
             href={"/"}
         >
             <div className=" hover:opacity-75 transition items-center gap-2 hidden md:flex" >
-                <Image
+                {/* <Image
                     src={"/next.svg"}
                     alt="Logo"
                     height={30}
                     width={30}
-                />
+                /> */}
                 <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)} >
                     Nodeforge
                 </p>
