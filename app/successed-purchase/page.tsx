508be784c0d128ee4a32d767a33eff055d5bce62
Minh Le <minh.le@c0x12c.com>
2024-06-23 09:36:52 +0700

    feat: update success purchase page

--- app/successed-purchase/page.tsx
+++ app/successed-purchase/page.tsx
@@ -9,8 +9,8 @@ import { useCallback } from "react";
 const SuccessedPurchase = () => {
     const router = useRouter();
 
-    const backHome = useCallback(() => {
-        router.push("/");
+    const backSubscription = useCallback(() => {
+        router.push("/subscription");
       }, [router]);
 
     return ( 
@@ -20,11 +20,11 @@ const SuccessedPurchase = () => {
               <CheckCircle className=" h-[80px] w-[80px] text-emerald-400" />
             </div>
             <div className="flex justify-center mt-8">
-              <p className=" text-3xl font-bold text-emerald-400 ">Thank you for buy Upgrade Subscription!</p>
+              <p className=" text-3xl font-bold text-emerald-400 ">Thank you for buying the service, please enjoy the rapid deployments!</p>
             </div>
             <div className=" flex justify-center mt-16 ">
-              <Button className="dark:md:hover:bg-grey-90" onClick={backHome}>
-                Back to Home
+              <Button className="dark:md:hover:bg-grey-90" onClick={backSubscription}>
+                Back to Subscription Page
               </Button>
             </div>
           </div>
